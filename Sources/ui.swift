c7a36b1978d2180a43538b1f41241611d464cd56
Steven Clukey <saclukey@live.com>
2016-08-17 00:38:48 -0400

    Remove the App class
    These functions are global to the UI library, so make them so

--- Sources/ui.swift
+++ Sources/ui.swift
@@ -2,10 +2,28 @@ import AppKit
 
 import clibui
 
-/*
-_UI_EXTERN void uiMsgBox(uiWindow *parent, const char *title, const char *description);
-_UI_EXTERN void uiMsgBoxError(uiWindow *parent, const char *title, const char *description);
-*/
+public struct InitOptions {
+	var options:clibui.uiInitOptions
+
+	public init() {
+		options = clibui.uiInitOptions()
+	}
+}
+
+public func initialize(options:inout InitOptions) -> String? {
+	if let output = clibui.uiInit(&options.options) {
+		return String(cString: output)
+	}
+	return nil
+}
+
+public func main() -> Void {
+	clibui.uiMain()
+}
+
+public func quit() -> Void {
+	clibui.uiQuit()
+}
 
 public func openFile(parent:Window) -> String? {
 	if let cPath = clibui.uiOpenFile(parent.op) {
@@ -28,181 +46,3 @@ public func messageBox(parent:Window, title:String, description:String) -> Void 
 public func messageBoxError(parent:Window, title:String, description:String) -> Void {
 	clibui.uiMsgBoxError(parent.op, title, description)
 }
-
-/*
-struct ui {
-
-	func smallStart() {
-		let app = App()
-
-		let w = Window(title:"New window", width:640, height:320, withMenu:true)
-		w.on(closing: {
-			print("New close handler")
-			app.quit()
-			return 0
-		})
-
-		w.on(contentSizeChanged: {
-			print("The size of the contents has changed!")
-		})
-
-		w.on(positionChanged: {
-			print("The position has changed!")
-			let (x, y) = w.position
-			print("X = \(x), Y = \(y)")
-		})
-
-		w.show()
-
-		print(w.title)
-
-		w.title = "This is the second title"
-		// w.center()
-		w.position = (100, 100)
-		w.margined = true
-
-		let box = Box(vertical:true)
-		box.padded = true
-
-
-		// let b = Button(text:"Button Text")
-
-		// b.on(clicked: {
-		// 	print("The button was clicked!")
-		// })
-
-		box.append(Label(text: "UI Test App"))
-
-		let openButton = Button(text: "Open File")
-		openButton.on(clicked: {
-			if let path = openFile(parent:w) {
-				print("Path: \(path)")
-			} else {
-				print("Cancelled")
-			}
-		})
-
-		box.append(openButton)
-
-
-
-
-
-
-		let msgButton = Button(text: "Send message")
-		msgButton.on(clicked: {
-			messageBox(parent:w, title:"Message title", description:"Message description")
-		})
-
-		box.append(msgButton)
-
-
-
-		let colorButton = ColorButton()
-		colorButton.on(changed: {
-			let (r,g,b,a) = colorButton.color
-			print("r = \(r), g = \(g), b = \(b), a = \(a)")
-		})
-
-		box.append(colorButton)
-
-
-
-
-
-		let cb = Checkbox(text: "This is a checkbox")
-		cb.on(toggled: {
-			let c = cb.checked ? "Yes" : "No"
-			print("Checked? \(c)")
-		})
-
-		cb.checked = true
-
-		box.append(cb)
-
-		let entry = Entry()
-		entry.on(changed: {
-			let currentText = entry.text
-			print("Current text: \(currentText)")
-		})
-		box.append(entry)
-
-		let slider = Slider(min:0, max:100)
-		slider.on(changed: {
-			print("Slider Currently: \(slider.value)")
-		})
-		box.append(slider)
-
-		let spinb = Spinbox(min:0, max:100)
-		spinb.on(changed: {
-			print("Spinbox Currently: \(spinb.value)")
-		})
-		box.append(spinb)
-
-		let pb = ProgressBar()
-		pb.value = 50
-		box.append(pb)
-
-		box.append(Separator(vertical:true))
-
-		let combo = Combobox()
-		combo.append(text: "First")
-		combo.append(text: "Second")
-		combo.append(text: "Third")
-		combo.on(selected: {
-			print("Selected: \(combo.selected)")
-		})
-		box.append(combo)
-
-		let ecombo = EditableCombobox()
-		ecombo.append(text: "First")
-		ecombo.append(text: "Second")
-		ecombo.append(text: "Third")
-		ecombo.on(changed: {
-			print("Editable value: \(ecombo.text)")
-		})
-		box.append(ecombo)
-
-		let radio = RadioButtons()
-		radio.append(text: "First")
-		radio.append(text: "Second")
-		radio.append(text: "Third")
-		radio.on(selected: {
-			print("Ticked: \(radio.selected)")
-		})
-		box.append(radio)
-
-		// This doesn't work for some reason
-		// let mle = MultilineEntry()
-		// mle.on(changed: {
-		// 	let currentText = mle.text
-		// 	print("Current text: \(currentText)")
-		// })
-		// box.append(mle)
-
-		box.append(DateTimePicker(type:Picker.Time))
-
-		w.set(child: box)
-		app.main()
-
-		// let c:Int = clibui.uiAllocControl(1, 1, 1, "Hey") as Int
-
-		// clibui.uiWindowFullscreen(24)
-
-		// var winStruct = clibui.uiWindow()
-
-		// var mainWindow = 
-
-		// var mainwin = clibui.uiControl(mainWindow)
-
-		// let mainWindowControl = mainWindow as uiControl?
-		// mainWindowControl.dynamicType.printClassName()
-
-		// clibui.uiControlShow(&(mainWindow! as uiControl));
-		// clibui.uiMain();
-
-	}
-
-    var text = "Hello, World!"
-}
-*/
