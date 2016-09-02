b8fd3d08e72d1dbe32fe5aa045cbae7c5b4be163
Steven Clukey <saclukey@live.com>
2016-09-02 10:04:26 -0400

    Implement `Brush` and `DrawPath`
    There are a lot of supporting structs and enums for the drawing
    functions, and unfortunately they come with some unfortunately
    necessary conversions, but whatever.
    
    So I think I’m leaning towards removing the `Draw` prefix, so
    `DrawPath` should probably become just `Path`, but I’m still not sure.

---
+++ Sources/Brush.swift
@@ -0,0 +1,274 @@
+import clibui
+
+public enum BrushType: Int {
+	case Solid
+	case LinearGradient
+	case RadialGradient
+	case Image
+}
+
+public enum LineCap: Int {
+	case Flat
+	case Round
+	case Square
+}
+
+public enum LineJoin: Int {
+	case Miter
+	case Round
+	case Bevel
+}
+
+public let DefaultMiterLimit = 10.0
+
+public enum FillMode: Int {
+	case Winding
+	case Alternate
+}
+
+public class DrawMatrix {
+	var cStruct:clibui.uiDrawMatrix
+
+	public var m11:Double;
+	public var m12:Double;
+	public var m21:Double;
+	public var m22:Double;
+	public var m31:Double;
+	public var m32:Double;
+
+	init(fromC cStruct:clibui.uiDrawMatrix) {
+		self.cStruct = cStruct
+		self.m11 = self.cStruct.M11
+		self.m12 = self.cStruct.M12
+		self.m21 = self.cStruct.M21
+		self.m22 = self.cStruct.M22
+		self.m31 = self.cStruct.M31
+		self.m32 = self.cStruct.M32
+	}
+
+	func update() -> Void {
+		self.m11 = self.cStruct.M11
+		self.m12 = self.cStruct.M12
+		self.m21 = self.cStruct.M21
+		self.m22 = self.cStruct.M22
+		self.m31 = self.cStruct.M31
+		self.m32 = self.cStruct.M32
+	}
+
+	public convenience init() {
+		let cStruct = clibui.uiDrawMatrix()
+		self.init(fromC:cStruct)
+	}
+
+	public func setIdentity() -> Void {
+		clibui.uiDrawMatrixSetIdentity(&self.cStruct)
+		self.update()
+	}
+
+	public func translate(origin:(x:Double, y:Double)) -> Void {
+		clibui.uiDrawMatrixTranslate(&self.cStruct, origin.x, origin.y)
+		self.update()
+	}
+}
+
+public class DrawBrush {
+	public var type:BrushType;
+
+	// solid brushes
+	public var r:Double;
+	public var g:Double;
+	public var b:Double;
+	public var a:Double;
+
+	// gradient brushes
+	public var x0:Double;		// linear: start X, radial: start X
+	public var y0:Double;		// linear: start Y, radial: start Y
+	public var x1:Double;		// linear: end X, radial: outer circle center X
+	public var y1:Double;		// linear: end Y, radial: outer circle center Y
+	public var outerRadius:Double;		// radial gradients only
+	public var stops:[BrushGradientStop];
+	private var cStops:UnsafeMutablePointer<clibui.uiDrawBrushGradientStop>
+	private var cNumStops:Int
+
+	init(fromC cStruct:clibui.uiDrawBrush) {
+		self.type = BrushType(rawValue: Int(cStruct.Type))!
+
+		self.r = cStruct.R
+		self.g = cStruct.G
+		self.b = cStruct.B
+		self.a = cStruct.A
+		self.x0 = cStruct.X0
+		self.y0 = cStruct.Y0
+		self.x1 = cStruct.X1
+		self.y1 = cStruct.Y1
+		self.outerRadius = cStruct.OuterRadius
+
+
+		self.stops = []
+		if let cStops = cStruct.Stops {
+			for i in 0..<cStruct.NumStops {
+				let stop = BrushGradientStop(fromC: cStops[i])
+				self.stops.append(stop)
+			}
+		}
+
+		// Initialize the self.cStops
+		self.cStops = UnsafeMutablePointer<clibui.uiDrawBrushGradientStop>(allocatingCapacity:cStruct.NumStops)
+		if cStruct.NumStops > 0 {
+			self.cStops.initializeFrom(cStruct.Stops, count:cStruct.NumStops)
+		}
+		self.cNumStops = cStruct.NumStops
+	}
+
+	public convenience init() {
+		let cStruct = clibui.uiDrawBrush()
+		self.init(fromC:cStruct)
+	}
+
+	deinit {
+		self.cStops.deinitialize()
+		self.cStops.deallocateCapacity(self.cNumStops)
+	}
+
+	private func updateCStops() -> Void {
+		// Remove the previous cStops
+		self.cStops.deinitialize()
+		self.cStops.deallocateCapacity(self.cNumStops)
+
+		// Create the new cStops
+		self.cStops = UnsafeMutablePointer<clibui.uiDrawBrushGradientStop>(allocatingCapacity:self.stops.count)
+		if self.stops.count > 0 {
+			var arrayOfCStructs:[clibui.uiDrawBrushGradientStop] = []
+			for thisStop in self.stops {
+				arrayOfCStructs.append(thisStop.toC())
+			}
+			cStops.initializeFrom(arrayOfCStructs, count:self.stops.count)
+		}
+		self.cNumStops = self.stops.count
+	}
+
+	public func toC() -> clibui.uiDrawBrush {
+		var cStruct = clibui.uiDrawBrush()
+		cStruct.Type = UInt32(self.type.rawValue)
+		cStruct.R = self.r
+		cStruct.G = self.g
+		cStruct.B = self.b
+		cStruct.A = self.a
+		cStruct.X0 = self.x0
+		cStruct.Y0 = self.y0
+		cStruct.X1 = self.x1
+		cStruct.Y1 = self.y1
+		cStruct.OuterRadius = self.outerRadius
+
+		updateCStops()
+		cStruct.Stops = self.cStops
+		cStruct.NumStops = self.cNumStops
+
+		return cStruct
+	}
+}
+
+public struct BrushGradientStop {
+	public var pos:Double;
+	public var r:Double;
+	public var g:Double;
+	public var b:Double;
+	public var a:Double;
+
+	init(fromC cStruct:clibui.uiDrawBrushGradientStop) {
+		self.pos = cStruct.Pos
+		self.r = cStruct.R
+		self.g = cStruct.G
+		self.b = cStruct.B
+		self.a = cStruct.A
+	}
+
+	func toC() -> clibui.uiDrawBrushGradientStop {
+		var cStruct = clibui.uiDrawBrushGradientStop()
+		cStruct.Pos = self.pos
+		cStruct.R = self.r
+		cStruct.G = self.g
+		cStruct.B = self.b
+		cStruct.A = self.a
+
+		return cStruct
+	}
+};
+
+public class StrokeParams {
+	public var cap:LineCap;
+	public var join:LineJoin;
+
+	public var thickness:Double;
+	public var miterLimit:Double;
+	public var dashes:[Double];
+
+	public var dashPhase:Double;
+
+	private var cDashes:UnsafeMutablePointer<Double>
+	private var cNumDashes:Int
+
+	init(fromC cStruct:clibui.uiDrawStrokeParams) {
+		self.cap = LineCap(rawValue: Int(cStruct.Cap))!
+		self.join = LineJoin(rawValue: Int(cStruct.Join))!
+
+		self.thickness = cStruct.Thickness
+		self.miterLimit = cStruct.MiterLimit
+
+		self.dashes = []
+		if let cDashes = cStruct.Dashes {
+			for i in 0..<cStruct.NumDashes {
+				self.dashes.append(cDashes[i])
+			}
+		}
+
+		self.dashPhase = cStruct.DashPhase
+
+		// Initialize the self.cDashes
+		self.cDashes = UnsafeMutablePointer<Double>(allocatingCapacity:cStruct.NumDashes)
+		if cStruct.NumDashes > 0 {
+			self.cDashes.initializeFrom(cStruct.Dashes, count:cStruct.NumDashes)
+		}
+		self.cNumDashes = cStruct.NumDashes
+	}
+
+	public convenience init() {
+		let cStruct = clibui.uiDrawStrokeParams()
+		self.init(fromC:cStruct)
+	}
+
+	deinit {
+		self.cDashes.deinitialize()
+		self.cDashes.deallocateCapacity(self.cNumDashes)
+	}
+
+	private func updateCDashes() -> Void {
+		// Remove the previous cDashes
+		self.cDashes.deinitialize()
+		self.cDashes.deallocateCapacity(self.cNumDashes)
+
+		// Create the new cDashes
+		self.cDashes = UnsafeMutablePointer<Double>(allocatingCapacity:self.dashes.count)
+		if self.dashes.count > 0 {
+			cDashes.initializeFrom(self.dashes)
+		}
+		self.cNumDashes = self.dashes.count
+	}
+
+	func toC() -> clibui.uiDrawStrokeParams {
+		var cStruct = clibui.uiDrawStrokeParams()
+
+		cStruct.Cap = UInt32(self.cap.rawValue)
+		cStruct.Join = UInt32(self.join.rawValue)
+		cStruct.Thickness = self.thickness
+		cStruct.MiterLimit = self.miterLimit
+		cStruct.DashPhase = self.dashPhase
+
+		updateCDashes()
+		cStruct.Dashes = self.cDashes
+		cStruct.NumDashes = self.cNumDashes
+
+		return cStruct
+	}
+}
+
---
+++ Sources/DrawPath.swift
@@ -0,0 +1,56 @@
+import clibui
+
+public class DrawPath {
+	let op:OpaquePointer
+
+	public init(fillMode:FillMode) {
+		self.op = clibui.uiDrawNewPath(UInt32(fillMode.rawValue))
+	}
+
+	deinit {
+		clibui.uiDrawFreePath(self.op)
+	}
+
+	public func newFigure(at point:(x:Double, y:Double)) -> Void {
+		clibui.uiDrawPathNewFigure(self.op, point.x, point.y)
+	}
+
+	public func newFigureWithArc(center:(x:Double, y:Double), radius:Double, startAngle:Double, sweep:Double, negative:Bool) -> Void {
+		clibui.uiDrawPathNewFigureWithArc(self.op, center.x, center.y, radius, startAngle, sweep, negative ? 1 : 0)
+	}
+
+	public func lineTo(point:(x:Double, y:Double)) -> Void {
+		clibui.uiDrawPathLineTo(self.op, point.x, point.y)
+	}
+
+	public func closeFigure() -> Void {
+		clibui.uiDrawPathCloseFigure(self.op)
+	}
+
+	public func end() -> Void {
+		clibui.uiDrawPathEnd(self.op)
+	}
+
+	public func arcTo(center:(x:Double, y:Double), radius:Double, startAngle:Double, sweep:Double, negative:Bool) -> Void {
+		clibui.uiDrawPathArcTo(self.op, center.x, center.y, radius, startAngle, sweep, negative ? 1 : 0)
+	}
+
+	public func bezierTo(c1:(x:Double, y:Double), c2:(x:Double, y:Double), end:(x:Double, y:Double)) -> Void {
+		clibui.uiDrawPathBezierTo(self.op, c1.x, c1.y, c2.x, c2.y, end.x, end.y)
+	}
+
+	public func addRectangle(at point:(x:Double, y:Double), withSize size:(width:Double, height:Double)) -> Void {
+		clibui.uiDrawPathAddRectangle(self.op, point.x, point.y, size.width, size.height)
+	}
+
+	public func stroke(withBrush brush:DrawBrush, andStroke stroke:StrokeParams, inContext context:DrawContext) -> Void {
+		var cBrush = brush.toC()
+		var cStroke = stroke.toC()
+		clibui.uiDrawStroke(context.op, self.op, &cBrush, &cStroke)
+	}
+
+	public func fill(withBrush brush:DrawBrush, inContext context:DrawContext) -> Void {
+		var cBrush = brush.toC()
+		clibui.uiDrawFill(context.op, self.op, &cBrush)
+	}
+}
