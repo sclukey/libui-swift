c7a435a9a0520864f8d626a122a31a05ba9afa39
Steven Clukey <saclukey@live.com>
2016-09-02 09:34:49 -0400

    Implement `FontButton` control
    Now that the `DrawText` stuff is here, the `FontButton` can be fully
    created.

---
+++ Sources/FontButton.swift
@@ -0,0 +1,34 @@
+import clibui
+
+public class FontButton: Control {
+	let op:OpaquePointer;
+	private var onChangedHandler: () -> Void
+
+	public init() {
+		self.op = clibui.uiNewFontButton()
+		self.onChangedHandler = {}
+
+		super.init(c: UnsafeMutablePointer(self.op))
+	}
+
+	public func getFont() -> DrawTextFont {
+		let cFont = clibui.uiFontButtonFont(self.op)
+
+		// I'm not sure how `cFont == nil` would ever happen since the value
+		// comes from a GUI picker that has no 'none' option. That said I am OK
+		// with this function throwing an error if `cFont!` fails.
+		return DrawTextFont(cFont!)
+	}
+
+	public func on(changed: () -> Void) -> Void {
+		onChangedHandler = changed
+		clibui.uiFontButtonOnChanged(self.op, { (w, d) -> Void in
+			if let selfPointer = d {
+				let myself = Unmanaged<FontButton>.fromOpaque(selfPointer).takeUnretainedValue()
+
+				myself.onChangedHandler()
+			}
+		}, UnsafeMutablePointer<Void>(Unmanaged.passUnretained(self).toOpaque()))		
+	}
+
+}
