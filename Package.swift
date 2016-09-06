c713bd82cfdc02691581ba8145655bd68c3a23dc
Steven Clukey <saclukey@live.com>
2016-09-06 00:20:53 -0400

    Use GitHub's clibui

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "ui",
     dependencies: [
-        .Package(url: "../clibui", majorVersion:1),
+        .Package(url: "https://github.com/sclukey/clibui.git", majorVersion:1),
     ]
 )
