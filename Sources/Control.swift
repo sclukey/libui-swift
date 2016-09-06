cbfee424beee79a52e826a9ae9ad34e10943ca50
Steven Clukey <saclukey@live.com>
2016-09-05 20:56:24 -0400

    Start adding some documentation comments
    Ideally LibUI would be documented, and this could just piggyback that.
    More importantly I suppose there should be a way to map between the C
    functions in LibUI and these ones here.

--- Sources/Control.swift
+++ Sources/Control.swift
@@ -1,23 +1,43 @@
 import clibui
 
+/// A user interface control
 public class Control {
+	/// Pointer to the C-level control structure
 	let control:UnsafeMutablePointer<clibui.uiControl>
+
+	/**
+	    Creates a Control wrapper from a pointer to the C-level structure
+	*/
 	public init(_ c: UnsafeMutablePointer<clibui.uiControl>) {
 		self.control = c
 	}
 
+	/**
+	    Free the C-level control's memory
+	*/
 	public func destroy() -> Void {
 		clibui.uiControlDestroy(self.control)
 	}
 
+	/**
+	    Get a pointer to the OS- and implementation-specific control
+	*/
 	public func handle() -> UnsafeMutableRawPointer {
 		return UnsafeMutableRawPointer(bitPattern:clibui.uiControlHandle(self.control))!
 	}
 
+	/**
+	    Honestly not sure what this function does and I don't feel like
+	    spending the time to figure it out. Perhaps I'll just wait for LibUI to
+	    be documented then update this.
+	*/
 	public func topLevel() -> Int {
 		return Int(clibui.uiControlToplevel(self.control))
 	}
 
+	/**
+	    The visibility of the control
+	*/
 	public var visible:Bool {
 		get {
 			return clibui.uiControlVisible(self.control) == 1
@@ -31,14 +51,23 @@ public class Control {
 		}
 	}
 
+	/**
+	    Show the control
+	*/
 	public func show() {
 		clibui.uiControlShow(self.control)
 	}
 
+	/**
+	    Hide the control
+	*/
 	public func hide() {
 		clibui.uiControlHide(self.control)
 	}
 
+	/**
+	    The enabled state of the control
+	*/
 	public var enabled:Bool {
 		get {
 			return clibui.uiControlEnabled(self.control) == 1
@@ -52,10 +81,16 @@ public class Control {
 		}
 	}
 
+	/**
+	    Enable the control
+	*/
 	public func enable() -> Void {
 		clibui.uiControlEnable(self.control)
 	}
 
+	/**
+	    Disable the control
+	*/
 	public func disable() -> Void {
 		clibui.uiControlDisable(self.control)
 	}
--- Sources/ui.swift
+++ Sources/ui.swift
@@ -4,19 +4,31 @@ import AppKit
 
 import clibui
 
+/// Helper for adding names to orientations
 public enum Orientation {
 	case Vertical
 	case Horizontal
 }
 
+/// Initialization options
 public struct InitOptions {
 	var options:clibui.uiInitOptions
 
+	/**
+	    Creates an instance with default initialization options
+	*/
 	public init() {
 		options = clibui.uiInitOptions()
 	}
 }
 
+/**
+    Initializes LibUI
+
+    - Parameter options: Initialization options.
+
+    - Returns: An error string on error, otherwise `nil`.
+*/
 public func initialize(options:inout InitOptions) -> String? {
 	if let output = clibui.uiInit(&options.options) {
 		return String(cString: output)
@@ -24,18 +36,37 @@ public func initialize(options:inout InitOptions) -> String? {
 	return nil
 }
 
+/**
+    Start the main event loop
+
+    This call does not return until LibUI quits
+*/
 public func main() -> Void {
 	clibui.uiMain()
 }
 
+/**
+    Destroy all object and free all memory used by LibUI
+*/
 public func uninit() -> Void {
 	clibui.uiUninit()
 }
 
+/**
+    Quit LibUI (i.e., stop the main event loop (i.e. return from `ui.main()`))
+*/
 public func quit() -> Void {
 	clibui.uiQuit()
 }
 
+/**
+    Open a dialog for selecting a file to open
+
+    - Parameter parent: Parent window for the dialog box
+
+    - Returns: The path of the selected file on success, or `nil` if the dialog
+      is canceled.
+*/
 public func openFile(parent:Window) -> String? {
 	if let cPath = clibui.uiOpenFile(parent.op) {
 		let filename = String(cString: cPath)
@@ -45,6 +76,16 @@ public func openFile(parent:Window) -> String? {
 	return nil
 }
 
+/**
+    Open a dialog for selecting a file to save
+
+    This one shows the overwrite warning and such.
+
+    - Parameter parent: Parent window for the dialog box
+
+    - Returns: The path of the file on success, or `nil` if the dialog is
+      canceled.
+*/
 public func saveFile(parent:Window) -> String? {
 	if let cPath = clibui.uiSaveFile(parent.op) {
 		let filename = String(cString: cPath)
@@ -60,6 +101,14 @@ class onShouldQuitHandlerStruct {
 	var onShouldQuitHandler: () -> Bool = { return true }
 }
 var osqh_obj = onShouldQuitHandlerStruct()
+
+/**
+    Defines what to do if the UI asks to quit
+
+    - Parameter shouldQuit: A function or closure that will be called when the
+      program asks to quit. This function return `true` if the program should
+      quit, or `false` if not.
+*/
 public func on(shouldQuit: @escaping () -> Bool) -> Void {
 	osqh_obj.onShouldQuitHandler = shouldQuit
 	clibui.uiOnShouldQuit({ (d) -> Int32 in
@@ -72,10 +121,26 @@ public func on(shouldQuit: @escaping () -> Bool) -> Void {
 	}, UnsafeMutableRawPointer(Unmanaged.passUnretained(osqh_obj).toOpaque()))
 }
 
+/**
+    Show a message box
+
+    - Parameter parent:      The parent window for the message box
+    - Parameter title:       The string to display in the title bar of the
+      message box
+    - Parameter description: The main message of the box
+*/
 public func messageBox(parent:Window, title:String, description:String) -> Void {
 	clibui.uiMsgBox(parent.op, title, description)
 }
 
+/**
+    Show a message box for an error
+
+    - Parameter parent:      The parent window for the message box
+    - Parameter title:       The string to display in the title bar of the
+      message box
+    - Parameter description: The main message of the box
+*/
 public func messageBoxError(parent:Window, title:String, description:String) -> Void {
 	clibui.uiMsgBoxError(parent.op, title, description)
 }
